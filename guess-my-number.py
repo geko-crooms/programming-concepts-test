bb3bf3a0ae7ac78226affc00880084e5b7eee8c2
Gökçe Aydos <18174744+goekce@users.noreply.github.com>
2026-09-13 12:14:58 +0200

    Add guessing logic to guess-my-number game
    Implement game logic for guessing the number with attempts.

--- guess-my-number.py
+++ guess-my-number.py
@@ -1,1 +1,27 @@
+"""
+Guess my number game.
+"""
 
+import random
+
+my_number = random.randrange(100)  # Picks a random number between 0 and 99.
+attempts = 0
+guess = int(input("Guess my number! It is from 0 to 99. You have 5 tries. "))
+
+### YOUR CODE HERE
+
+
+# Use the following lines
+        print("Too low")
+    print("Out of tries ― the number was ")
+    print("You got it!")
+    attempts += 1
+else:
+    guess = int(input("Guess my number! "))
+while attempts < 5 and guess != my_number:
+        print("Too high.")
+    if guess > my_number:
+    else:
+    print(my_number)
+if guess == my_number:
+  
